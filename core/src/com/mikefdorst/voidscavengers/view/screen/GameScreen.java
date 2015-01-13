fff1a386b5ffd6ae91f9db61acfa4d413ad3c38d
anthropomorphic <mikefdorst@mikefdorst.com>
2015-01-12 20:21:35 -0800

    Set up player body

--- core/src/com/mikefdorst/voidscavengers/view/screen/GameScreen.java
+++ core/src/com/mikefdorst/voidscavengers/view/screen/GameScreen.java
@@ -1,22 +1,64 @@
 package com.mikefdorst.voidscavengers.view.screen;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.*;
 import com.mikefdorst.voidscavengers.VoidScavengers;
+import com.mikefdorst.voidscavengers.util.reference.Ref;
+import com.mikefdorst.voidscavengers.view.shape.EquilateralTriangle;
 
 public class GameScreen implements Screen {
   
   final VoidScavengers game;
+  private OrthographicCamera camera;
+  private World world;
+  private Box2DDebugRenderer renderer;
+  private Body body;
+  private float world_scale;
   
   public GameScreen(VoidScavengers game) {
     this.game = game;
+    
+    camera = new OrthographicCamera();
+    world_scale = 1/5f;
+    camera.setToOrtho(false, view_width(), view_height());
+    world = new World(new Vector2(0, 0), true);
+    renderer = new Box2DDebugRenderer();
+    
+    BodyDef bodyDef = new BodyDef();
+    bodyDef.type = BodyDef.BodyType.DynamicBody;
+    bodyDef.position.set(view_width()/2, view_height()/2);
+    body = world.createBody(bodyDef);
+    
+    FixtureDef fixtureDef = new FixtureDef();
+    
+    PolygonShape triangle = new PolygonShape();
+    triangle.set(new EquilateralTriangle(10).getVertices());
+    fixtureDef.shape = triangle;
+    fixtureDef.density = 1f;
+    fixtureDef.friction = 0.5f;
+    fixtureDef.restitution = 0.5f;
+    body.createFixture(fixtureDef);
+    
+    triangle.dispose();
+  }
+  
+  private float view_height() {
+    return Ref.window.height * world_scale;
+  }
+  
+  private float view_width() {
+    return Ref.window.width * world_scale;
   }
 
   @Override
   public void render(float delta) {
-    
+    Gdx.gl.glClearColor(0, 0, 0, 1);
+    Gdx.gl.glClear(Gdx.gl.GL_COLOR_BUFFER_BIT);
+    renderer.render(world, camera.combined);
+    world.step(1/60f, 6, 2);
   }
 
   @Override
